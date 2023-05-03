9c3e083aa401c3f9fdcf757953e24638ffe08c06
Mark Torok <marktorok270@gmail.com>
2023-05-03 20:20:49 +0100

    up

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-const navButton = document.querySelector('.menu');
-const navigation = document.querySelector('.nav-items');
-console.log(navButton);
-console.log(navigation);
+const navButton = document.querySelectorAll('.hamburger')
+const hamburger = document.querySelector('.menu')
+const navigation = document.querySelector('.nav-items')
+// console.log(navButton)
+// console.log(navigation)
 
-navButton.addEventListener('click', () => {
-    navigation.classList.toggle('show');
-    if (navButton.getAttribute('aria-expanded') === 'false') {
-        navButton.setAttribute('aria-expanded', 'true')
-    } else {
-        navButton.setAttribute('aria-expanded', 'false');
-        navigation.classList.remove('show')
-    }
-});
+hamburger.addEventListener('click', () => {
+  navigation.classList.toggle('active')
+  for (let i = 0; i < navButton.length; i++) {
+    console.log(navButton[i])
+    navButton[i].classList.toggle('hidden')
+  }
+})
 
+navigation.addEventListener('click', () => {
+  navigation.classList.remove('active')
+})
 
+// navButton.addEventListener('click', () => {
+//     navigation.classList.toggle('show');
+//     if (navButton.getAttribute('aria-expanded') === 'false') {
+//         navButton.setAttribute('aria-expanded', 'true')
+//     } else {
+//         navButton.setAttribute('aria-expanded', 'false');
+//         navigation.classList.remove('show')
+//     }
+// });
